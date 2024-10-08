0117eb9b7d16c86f941805ab6e01c81b403f5848
254In61 <allanmaseghe4@gmail.com>
2024-10-08 17:22:09 +1000

    Updates @ Tue Oct  8 17:22:09 AEST 2024

--- run-play.sh
+++ run-play.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
+echo "" && echo "==> Delete existing roles" && echo ""
 rm -rf $HOME/.ansible/roles
+
+echo "" && echo "==> Set env variables" && echo ""
+source ../env-vars
+
 cd aap-objects-build
 git pull
-source ../env-vars
+
+echo "" && echo "==> Pull down roles from git" && echo ""
 ansible-galaxy install -r roles/github-requirements.yml
+
+echo "" && echo "==> Run playbook" && echo ""
 ansible-playbook site.yml
