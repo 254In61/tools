1aebbd944a45498fca8fa0abb5e06062f4ada5fa
254In61 <allanmaseghe4@gmail.com>
2024-10-09 13:04:00 +1000

    Updates @ Wed Oct  9 13:04:00 AEST 2024

--- run-play.sh
+++ run-play.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
+git_repo=juniper-software-upgrade
+playbook=pre-checks.yml
+
 echo "" && echo "==> Delete existing roles" && echo ""
 rm -rf $HOME/.ansible/roles
 
 echo "" && echo "==> Set env variables" && echo ""
 source env-vars
 
-cd aap-objects-build
-git pull
+git clone https://github.com/254In61/$git_repo.git
+cd $git_repo
 
 echo "" && echo "==> Pull down roles from git" && echo ""
-ansible-galaxy install -r roles/github-requirements.yml
+ansible-galaxy install -r roles/requirements.yml
 
 echo "" && echo "==> Run playbook" && echo ""
-ansible-playbook site.yml
+ansible-playbook $playbook -i inventory/staging.ini -e "@secrets.yml"
+
+echo "" && echo "==> Clean up" && echo ""
+cd ..
+rm -rf $git_repo
